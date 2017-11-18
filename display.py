825267591ecfaa6cc316674649f72bec7692af5f
rock <git@rockshouse.com>
2017-11-18 14:20:14 -0500

    of course there are no running orange line trains

--- display.py
+++ display.py
@@ -1,26 +1,41 @@
 import atexit
+import math
+import os
+import time
+
 import Image
 import ImageDraw
 import ImageFont
-import os
-import time
-import math
-from predict import predict
 
+from predictor import predictor
 from rgbmatrix import Adafruit_RGBmatrix
 
-
 # Configuration
-fps            = 5
+api_key = 'wX9NwuHnZU2ToO7GmGR9uw'
+place = 'place-grnst'
+filter = 'Oak Grove'
+station_label = "GreenSt. North "
+
+place = 'place-sstat'
+filter = 'Alewife'
+station_label = "North to Alewife"
+
+fps            = 8
 width          = 64  # Matrix size (pixels) -- change for different matrix
 height         = 32  # types (incl. tiling).  Other code may need tweaks.
 matrix         = Adafruit_RGBmatrix(32, 2) # rows, chain length
 
-orangeLineOrange     = (255, 130, 0) # Color for route labels (usu. numbers)
-black = (0, 0, 0)
-red = (255, 0, 0)
-white = (255, 255, 255)
-greenLineGreen = (66, 134, 8)
+green          = (0, 132, 69)
+red            = (225, 39, 38)
+orange         = (232, 116, 36)
+blue           = (15, 75, 145)
+silver         = (125, 134,140)
+plum           = (194, 147, 181)
+aqua           = (216, 240, 252)
+white          = (255, 255, 255)
+black          = (0, 0, 0)
+
+themecolor     = red
 font           = ImageFont.load(os.path.dirname(os.path.realpath(__file__)) + '/helvR08.pil')
 
 # Main application -----------------------------------------------------------
@@ -33,37 +48,51 @@ draw        = ImageDraw.Draw(image)
 # fiddle with some code while LEDs are blinding you.
 def clearOnExit():
 	matrix.Clear()
-
 atexit.register(clearOnExit)
 
-train_stop = predict()
+train_stop_predictor = predictor(api_key, place, filter)
 
 currentTime = 0.0
 prevTime    = 0.0
+error = None
+errlen = 0
 # Event loop
 while True:
-	data = train_stop.gettraintimes()
-	print data
+	data = {}
+	try:
+		data = train_stop_predictor.get_train_times()
+	except ValueError as e:
+		error = str(e)
+		if errlen <= 0:
+			errlen = float(font.getsize(error)[0])
+			errlen2 = errlen
+			errlen += 64
 	# Draw Stuff
-	draw.rectangle((0, 0, width, height), fill=orangeLineOrange)
-	draw.rectangle((1, 1, width-2, height-2), fill=black)
-	#draw.rectangle((1, 1, width-2, 10), fill=white)
+	draw.rectangle((0, 0, width, height), fill=black)
 
-	draw.line((0, 10, width, 10), fill=orangeLineOrange)
+	if bool(error) and errlen > 0:
+		draw.text((6, 10), "No Train Data.", font=font, fill=white)
+		draw.text((1 - (errlen2 - errlen), 20), error, font=font, fill=red)
+		errlen = errlen - .75
 
-	for label, times in data.items():
-		times.sort()
-		times = [x / 60 for x in times[0:3]]
-		minutes = []
-		for duration in times:
-			minutes.append(int(math.ceil((duration))))
+	else:
+		time_label = time.strftime("%b %d %H:%M")
+		draw.text((5, 20),time_label, font=font, fill=green)
+		for label, times in data.items():
+			times.sort()
+			times = [x / 60 for x in times[0:4]]
+			minutes = []
+			for duration in times:
+				minutes.append(int(math.ceil((duration))))
+			draw.text((2, 10), ",".join(map(str, minutes)) +" mins. ", font=font, fill=white)
 
-		print minutes
-		draw.text((2, 0), "To:  "+label, font=font, fill=orangeLineOrange)
-		draw.text((2, 10), ", ".join(map(str, minutes)) +" mins. ", font=font, fill=white)
+	draw.text((1, 0), station_label, font=font, fill=themecolor)
 
-	draw.text((5, 20), "   No Delays! ", font=font, fill=greenLineGreen)
-
+	draw.line((0, 0, width, 0), fill=themecolor) #top
+	draw.line((0, 10, width, 10), fill=themecolor) #middle
+	draw.line((0, height-1, width, height-1), fill=themecolor)  # bottom
+	draw.line((0, 0, 0, height), fill=themecolor)  # left
+	draw.line((width - 1, 0, width - 1, height), fill=themecolor)  # right
 
 	# Timing
 	currentTime = time.time()
@@ -74,6 +103,3 @@ while True:
 
 	# Offscreen buffer is copied to screen
 	matrix.SetImage(image.im.id, 0, 0)
-
-
-text = raw_input("Press enter to exit...")
---
+++ predictor.py
@@ -0,0 +1,54 @@
+
+import urllib
+import json
+import time
+
+class predictor:
+    __api_key = None
+    __json = {}
+    __last_fetch = 0.0
+    __place = None
+    __filter = None
+
+    def __init__(self, api_key, place, filter = None):
+        self.__api_key = api_key
+        self.__place = place
+        self.__filter = filter
+
+    def __get_data(self):
+        data = None
+        try:
+            connection = urllib.urlopen('http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key='+self.__api_key +'&stop='+self.__place+'&format=json')
+            data = json.loads(connection.read())
+            connection.close()
+
+        finally:
+            return data
+
+    def get_train_times(self):
+        currentTime = time.time()
+        data = {}
+        if (not bool(self.__json) or currentTime - self.__last_fetch > 15):
+            self.__json = self.__get_data()
+            self.__last_fetch = currentTime
+
+        if not "mode" in self.__json:
+            message = 'Cannot process data feed from MBTA.'
+            if "alert_headers" in self.__json and len(self.__json["alert_headers"]) > 0:
+                message = ""
+                for alert in self.__json["alert_headers"]:
+                    message += alert["header_text"]+"  "
+            raise ValueError(message)
+
+        for mode in self.__json["mode"]:
+            if mode["mode_name"] == "Subway":
+                for route in mode["route"]:
+                    for direction in route["direction"]:
+                        for trip in direction["trip"]:
+                            if "trip_headsign" in trip:
+                                if(bool(self.__filter) and self.__filter != trip["trip_headsign"]):
+                                    continue
+                                if trip["trip_headsign"] not in data:
+                                    data[trip["trip_headsign"]] = []
+                                data[trip["trip_headsign"]].append(float(trip["pre_away"]))
+        return data
