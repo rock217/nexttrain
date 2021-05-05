f9c70517e05c1e13979dccde91b34fe7474ea51f
rock217 <github@rockshouse.com>
2021-05-05 18:34:58 -0400

    whitespace

--- display.py
+++ display.py
@@ -43,17 +43,17 @@ draw        = ImageDraw.Draw(image)
 # Clear matrix on exit.  Otherwise it's annoying if you need to break and
 # fiddle with some code while LEDs are blinding you.
 def clearOnExit():
-	matrix.Clear()
+    matrix.Clear()
 atexit.register(clearOnExit)
 train_stop_predictor = predictor(api_key, place, filter)
 
 def drawBox():
-	draw.line((0, 0, width, 0), fill=themecolor)  # top
-	draw.line((0, 10, width, 10), fill=themecolor)  # middle
+    draw.line((0, 0, width, 0), fill=themecolor)  # top
+    draw.line((0, 10, width, 10), fill=themecolor)  # middle
 
-	draw.line((0, height - 1, width, height - 1), fill=themecolor)  # bottom
-	draw.line((0, 0, 0, height), fill=themecolor)  # left
-	draw.line((width - 1, 0, width - 1, height), fill=themecolor)  # right
+    draw.line((0, height - 1, width, height - 1), fill=themecolor)  # bottom
+    draw.line((0, 0, 0, height), fill=themecolor)  # left
+    draw.line((width - 1, 0, width - 1, height), fill=themecolor)  # right
 
 
 # Splash Screen'
@@ -70,35 +70,35 @@ prevSaveTime    = 0.0
 
 # Event loop
 def loop():
-	global prevTime, prevSaveTime
-	currentTime = time.time()
-	data = {}
-	error = None
+    global prevTime, prevSaveTime
+    currentTime = time.time()
+    data = {}
+    error = None
 
-	try:
-		data = train_stop_predictor.get_train_times()
-	except ValueError as e:
-		error = str(e)
+    try:
+        data = train_stop_predictor.get_train_times()
+    except ValueError as e:
+        error = str(e)
 
-	# Draw Stuff
-	draw.rectangle((0, 0, width, height), fill=black)
+    # Draw Stuff
+    draw.rectangle((0, 0, width, height), fill=black)
 
-	if bool(error):
-		draw.text((6, 10), "No Train Data.", font=font, fill=white)
-		draw.text((1, 20), error, font=font, fill=red)
+    if bool(error):
+        draw.text((6, 10), "No Train Data.", font=font, fill=white)
+        draw.text((1, 20), error, font=font, fill=red)
 
-	else:
-		drawBox()
-		draw.text((1, 0), station_label, font=font, fill=yellow)
+    else:
+        drawBox()
+        draw.text((1, 0), station_label, font=font, fill=yellow)
 
-		date_label = time.strftime("%b %d")
-		datex = 32 + ((31 - float(font.getsize(date_label)[0])) / 2)
-		draw.text((datex, 21), date_label, font=font, fill=green)
+        date_label = time.strftime("%b %d")
+        datex = 32 + ((31 - float(font.getsize(date_label)[0])) / 2)
+        draw.text((datex, 21), date_label, font=font, fill=green)
 
-		separator = ":" if int(time.time()) % 2 == 0 else " "
-		time_label = time.strftime("%-I" + separator + "%M")
-		timex = 1 + ((31 - float(font.getsize(time_label)[0])) / 2)
-		draw.text((timex, 21), time_label, font=font, fill=green)
+        separator = ":" if int(time.time()) % 2 == 0 else " "
+        time_label = time.strftime("%-I" + separator + "%M")
+        timex = 1 + ((31 - float(font.getsize(time_label)[0])) / 2)
+        draw.text((timex, 21), time_label, font=font, fill=green)
 
         data["trains"].sort()
         times = [x for x in data["trains"][0:3]]
@@ -120,21 +120,21 @@ def loop():
         draw.text((xoff, 10), redminuteslabel , font=font, fill=red)
         draw.text((xoff+font.getsize(redminuteslabel)[0], 10), whiteminuteslabel, font=font, fill=white)
 
-	# Timing
-	timeDelta = (1.0 / fps) - (currentTime - prevTime)
-	if (timeDelta > 0.0):
-		time.sleep(timeDelta)
-	prevTime = currentTime
-	# Offscreen buffer is copied to screen
-	matrix.SetImage(image.im.id, 0, 0)
-	if(currentTime - prevSaveTime > 60):
-		#image.save("/var/www/html/train.png")
-		prevSaveTime = currentTime
-		memory_info = 'Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-		print memory_info
-		#wr = open('/var/www/html/memory.txt', 'w')
-		#wr.write(memory_info)
-		#wr.close()
+    # Timing
+    timeDelta = (1.0 / fps) - (currentTime - prevTime)
+    if (timeDelta > 0.0):
+        time.sleep(timeDelta)
+    prevTime = currentTime
+    # Offscreen buffer is copied to screen
+    matrix.SetImage(image.im.id, 0, 0)
+    if(currentTime - prevSaveTime > 60):
+        #image.save("/var/www/html/train.png")
+        prevSaveTime = currentTime
+        memory_info = 'Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        print memory_info
+        #wr = open('/var/www/html/memory.txt', 'w')
+        #wr.write(memory_info)
+        #wr.close()
 
 while True:
-	loop()
+    loop()
